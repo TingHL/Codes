591a079809992afc936363bbf534e5a7f77a2850
TingHL <2840168867@qq.com>
2020-11-21 14:14:53 +0800

    Spring demo1

--- JavaLearningPractise/src/LeetCode/Solution.java
+++ JavaLearningPractise/src/LeetCode/Solution.java
@@ -2,30 +2,41 @@ package LeetCode;
 
 import javax.swing.plaf.SliderUI;
 
-public class Solution {
-    public int removeDuplicates(int[] nums) {
-        int index=0;
-        int currentNum=Integer.MAX_VALUE;
-        int times=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==currentNum&&times<=1){
-                nums[index]=nums[i];
-                index++;
-                times++;
-            }else if(nums[i]==currentNum&&times>=2){
+class Solution {
+    public int strToInt(String string) {
+        String str=string.trim();
+        boolean sign=true;
+        int i=0;
+        StringBuilder sb=new StringBuilder();
 
-            }else if(nums[i]!=currentNum){
-                nums[index]=nums[i];
-                times=1;
-                index++;
+
+        if(str.charAt(i)<='9'&&str.charAt(i)>='0'){
+            while(i<str.length()&&str.charAt(i)<='9'&&str.charAt(i)>='0'){
+                sb.append(str.charAt(i));
+                ++i;
             }
-        }
-        return index;
+        }else if(str.charAt(i)=='+'||str.charAt(i)=='-'){
+            if(i+1<str.length()&&str.charAt(i+1)<='9'&&str.charAt(i+1)>='0'){
+                if(str.charAt(i)=='-')sign=false;
+                i++;
+                while(i<str.length()&&str.charAt(i)<='9'&&str.charAt(i)>='0'){
+                    sb.append(str.charAt(i));
+                    i++;
+                }
+            }else return 0;
+        }else return 0;
+
+
+        long num=Long.parseLong(sb.toString());
+        if(!sign)num=-num;
+        if(num<Integer.MIN_VALUE)return Integer.MIN_VALUE;
+        if(num>Integer.MAX_VALUE)return Integer.MAX_VALUE;
+        return Integer.parseInt(sb.toString());
     }
 
     public static void main(String[] args) {
         Solution solution=new Solution();
-        int[] data={1,1,1,2,2,3};
-        solution.removeDuplicates(data);
+        String data=" ";
+        solution.strToInt(data);
     }
 }
